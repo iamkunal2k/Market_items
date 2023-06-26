e6f134dbaec4bd9b8adc27f86eedefb8473698a3
iamkunal2k <kunalkumbhare8@gmail.com>
2023-06-26 18:47:08 +0530

    Created a function for createProducts and tested success test cases

--- src/contracts/Market_items.sol
+++ src/contracts/Market_items.sol
@@ -3,10 +3,47 @@ pragma solidity ^0.5.0;
 contract Market_items {
     string public name;
 
-    //Constructor
+    //Initial count of Product is set to 0
+    uint public productCount = 0;
+    mapping(uint => Product) public products;
 
+    // product params
+    struct Product {
+        uint id;
+        string name;
+        uint price;
+        address owner;
+        bool purchase;
+    }
+
+    event ProductCreated(
+        uint id, 
+        string name,
+        uint price,
+        address owner,
+        bool purchased
+    );
+
+    //Constructor
     constructor() public {
         name = "Market items";
+    }
+
+    function createProduct(string memory _name, uint _price) public {
+
+        //Require a name
+        require(bytes(name).length > 0);
+
+        //Require a valid price
+        require(_price > 0);
+        //Increment productCount
+        productCount ++;
+        
+        //create the product
+        products[productCount] = Product(productCount, _name, _price, msg.sender, false);
+        
+        //trigger an event
+        emit ProductCreated(productCount, _name, _price, msg.sender, false);
     }
 }
 
--- test/Market_items.test.js
+++ test/Market_items.test.js
@@ -1,8 +1,12 @@
 const { assert } = require("chai");
 
+require('chai')
+.user(require('chai-as-promised'))
+.should()
+
 const Market_items = artifacts.require("./Market_items.sol");
 
-contract('Market_items', (accounts) => {
+contract('Market_items', ([deployer, seller, buyer]) => {
     let market_items
 
     before(async () => {
@@ -23,5 +27,37 @@ contract('Market_items', (accounts) => {
             assert.equal(name, 'Market items')
         })
     })
+
+    describe('products', async () => {
+        let result, productCount
+
+        before(async () => {
+            result = await market_items.createProduct('iPhone14', web3.utils.toWei('1', 'Ether'), {from: seller})  //Price is expressed in Wei, so for 1 ether = 1 followed by (0x18)
+            productCount = await market_items.productCount()
+        })
+
+        it('creates products', async () => {
+            //SUCCESS
+            assert.equal(productCount, 1)
+            // to fetch all details of the product >> console.log(result.logs)
+            const event = result.logs[0].args
+            assert.equal(event.id.toNumber(), productCount.toNumber(), "id is correct")
+            assert.equal(event.name, "iPhone14" , 'name is correct')
+            assert.equal(event.price, 1000000000000000000 , 'price is correct')
+            assert.equal(event.owner, seller, 'is correct')
+            assert.equal(event.purchased, false, ' purchased is correct')
+        
+        //FAILURE
+        })
+        
+    })
+
+
+
+
+
+
+
+
 })
 
